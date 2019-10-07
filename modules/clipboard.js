cb96fd6a84348a016113e5534a458a03ca597df7
andrei-cacio <andrei@moqups.com>
2019-10-07 17:39:41 +0300

    Removed white-space normal when converting HTML to Quill, it only causes problems. Also removed the clipboard convert logic which collapses white spaces for non pre styled nodes

--- modules/clipboard.js
+++ modules/clipboard.js
@@ -405,23 +405,7 @@ function matchText(node, delta) {
   if (text.trim().length === 0 && node.parentNode.classList.contains('ql-clipboard')) {
     return delta;
   }
-  if (!computeStyle(node.parentNode).whiteSpace.startsWith('pre')) {
-    // eslint-disable-next-line func-style
-    let replacer = function(collapse, match) {
-      match = match.replace(/[^\u00a0]/g, '');    // \u00a0 is nbsp;
-      return match.length < 1 && collapse ? ' ' : match;
-    };
-    text = text.replace(/\r\n/g, ' ').replace(/\n/g, ' ');
-    text = text.replace(/\s\s+/g, replacer.bind(replacer, true));  // collapse whitespace
-    if ((node.previousSibling == null && isLine(node.parentNode)) ||
-        (node.previousSibling != null && isLine(node.previousSibling))) {
-      text = text.replace(/^\s+/, replacer.bind(replacer, false));
-    }
-    if ((node.nextSibling == null && isLine(node.parentNode)) ||
-        (node.nextSibling != null && isLine(node.nextSibling))) {
-      text = text.replace(/\s+$/, replacer.bind(replacer, false));
-    }
-  }
+ 
   return delta.insert(text);
 }
 
